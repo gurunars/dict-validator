733bf1a41be013befabfcd5618a41923f19240fc
Anton Berezin <gurunars@gmail.com>
2017-05-14 09:06:24 +0300

    Travis ci

--- build-scripts/publish.sh
+++ build-scripts/publish.sh
@@ -2,9 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Travis specific
-if [ -f ${DIR}/pypirc ]; then
-    cp ${DIR}/pypirc ~/.pypirc
-fi
-
 python setup.py sdist upload -r pypi
--- build-scripts/publish_docs.sh
+++ build-scripts/publish_docs.sh
@@ -4,13 +4,6 @@ set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Travis specific
-if [ -f ${DIR}/deploy_key ]; then
-    chmod 600 ${DIR}/deploy_key
-    eval $(ssh-agent -s)
-    ssh-add ${DIR}/deploy_key
-fi
-
 ORIGIN=$(git config remote.origin.url)
 ORIGIN=${ORIGIN/https:\/\/github.com\//git@github.com:}
 ORIGIN=${ORIGIN%/}
@@ -20,7 +13,7 @@ NAME=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
 cp -ar .docs/html /tmp/${NAME}
 cd /tmp/${NAME}
 git init
-git config user.email "publisher@travis"
+git config user.email "publisher@ci-machine"
 git config user.name "Publisher"
 git checkout -b gh-pages
 touch .nojekyll
